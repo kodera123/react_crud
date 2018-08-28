9770a92ec19fefd6cebf62f001744ee8a6f6ee16
kodera123 <b.b.015.k.k@softbank.ne.jp>
2018-08-28 09:24:48 +0900

    app/views/

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,6 @@ console.log('Hello World from Webpacker')
 var componentRequireContext = require.context("components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
+
+import Counter from '../counter'
+window.Counter = Counter
