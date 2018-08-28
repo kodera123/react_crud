f7ff1244c911bb5f622e3acd32d85138af44e260
kodera123 <b.b.015.k.k@softbank.ne.jp>
2018-08-28 09:26:44 +0900

    redux で counter を実現する

---
+++ app/javascript/counter/reducers/count.js
@@ -0,0 +1,25 @@
+import {
+  COUNT_UP,
+  COUNT_DOWN
+} from '../constants/action_types'
+
+const initialState = {
+  count: 0
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case COUNT_UP:
+      return {
+        ...state,
+        count: ++state.count
+      }
+    case COUNT_DOWN:
+      return {
+        ...state,
+        count: --state.count
+      }
+    default:
+      return state
+  }
+}
