e8c9a0524c798411da8280d0225a700592519ed2
kodera123 <b.b.015.k.k@softbank.ne.jp>
2018-08-30 21:27:42 +0900

    file 作る場所間違えた component -> counter/component

--- app/javascript/components/counter.jsx
+++ app/javascript/components/counter.jsx
@@ -2,46 +2,41 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Counter extends Component {
-  // コンポーネントが自身の状態をstateに持つ時代は終わりを迎えました。
-  // constructor(props) {
-  //   super(props)
+  constructor(props) {
+    super(props)
 
-  //   this.state = {
-  //     count: props.count
-  //   }
-  // }
+    this.state = {
+      count: props.count
+    }
+  }
 
-  // handleUp = () => {
-  //   this.setState({
-  //     count: ++this.state.count
-  //   })
-  // }
+  handleUp = () => {
+    this.setState({
+      count: ++this.state.count
+    })
+  }
 
-  // handleDown = () => {
-  //   this.setState({
-  //     count: --this.state.count
-  //   })
-  // }
+  handleDown = () => {
+    this.setState({
+      count: --this.state.count
+    })
+  }
 
   render() {
     return (
       <div>
-        <div>{this.props.count}</div>
-        <button onClick={this.props.onClickCountUp}>+</button>
-        <button onClick={this.props.onClickCountDown}>-</button>
+        <div>{this.state.count}</div>
+        <button onClick={this.handleUp}>+</button>
+        <button onClick={this.handleDown}>-</button>
       </div>
     )
   }
 }
 
-// これもreduxで賄える
-// Counter.defaultProps = {
-//   count: 0
-// }
-
+Counter.defaultProps = {
+  count: 0
+}
 
 Counter.PropTypes = {
-  count: PropTypes.number.isRequired,
-  onClickCountUp: PropTypes.func.isRequired,
-  onClickCountDown: PropTypes.func.isRequired
+  count: PropTypes.number
 }
